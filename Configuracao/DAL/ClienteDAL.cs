f6369b17ab6dbe3fc428bcb9ef91e6c0a624f040
Luis Fernando <101011199+Luisitu1@users.noreply.github.com>
2023-06-07 21:46:04 -0300

    inclusao da classe cliente

---
+++ Configuracao/DAL/ClienteDAL.cs
@@ -0,0 +1,191 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ClienteDAL
+    {
+        public void Inserir(Cliente _cliente)
+        {
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            try
+            {
+                SqlCommand cmd = cn.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Ciente(Nome, CPF, RG, Email, Fone) VALUES(@Nome, @CPF, @RG, @Email, @Fone)";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+                cmd.Parameters.AddWithValue("@Nome", _cliente.Nome);
+                cmd.Parameters.AddWithValue("@CPF", _cliente.CPF);
+                cmd.Parameters.AddWithValue("@RG", _cliente.RG);
+                cmd.Parameters.AddWithValue("@Email", _cliente.Email);
+                cmd.Parameters.AddWithValue("@Fone", _cliente.Fone);
+
+                cmd.Connection = cn;
+                cn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um clienteno banco de dados.", ex) { Data = { { "Id", 15 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public List<Cliente> BuscarTodos()
+
+        {
+            SqlConnection cn = new SqlConnection();
+            Cliente cliente = new Cliente();
+
+            try
+            {
+               
+                SqlCommand cmd = new SqlCommand();
+               
+
+                List<Cliente> clienteList = new List<Cliente>();
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT Nome, CPF, RG, Email, Fone FROM Cliente";
+                cmd.CommandType = System.Data.CommandType.Text;
+
+
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        cliente.Id = (int)rd["Id"];
+                        cliente.Nome = rd["Nome"].ToString();
+                        cliente.CPF = rd["CPF"].ToString();
+                        cliente.RG = rd["RG"].ToString();
+                        cliente.Email = rd["Email"].ToString();
+                        cliente.Fone = rd["Fone"].ToString();
+
+                        clienteList.Add(cliente);
+                    }
+                }
+                return clienteList;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ocorreu um erro ao tentar buscar clientes no banco de dados", ex) { Data = { { "Id", 16 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public List<Cliente> BuscarPorNome(string _nome)
+        {
+            SqlConnection cn = new SqlConnection();
+            Cliente cliente = new Cliente();
+            List<Cliente> clienteList = new List<Cliente>();
+            try
+            {
+                
+                SqlCommand cmd = new SqlCommand();
+               
+
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT Nome, CPF, RG, Email, Fone FROM Cliente WHERE Nome Like @Nome";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", "%" + _nome + "%");
+
+
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        cliente.Id = (int)rd["Id"];
+                        cliente.Nome = rd["Nome"].ToString();
+                        cliente.CPF = rd["CPF"].ToString();
+                        cliente.RG = rd["RG"].ToString();
+                        cliente.Email = rd["Email"].ToString();
+                        cliente.Fone = rd["Fone"].ToString();
+
+                        clienteList.Add(cliente);
+                    }
+                }
+                return clienteList;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ocorreu um erro ao tentar buscar clientes por id no banco de dados", ex) { Data = { { "Id", 17 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Cliente BuscarPorId(int _id)
+        {
+            SqlConnection cn = new SqlConnection();
+            Cliente cliente = new Cliente();
+            
+            try
+            {
+
+                SqlCommand cmd = new SqlCommand();
+
+
+                cmd.Connection = cn;
+                cmd.CommandText = @"SELECT Nome, CPF, RG, Email, Fone FROM Cliente WHERE Id = @Id";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@Id", _id);
+
+
+                cn.Open();
+                using (SqlDataReader rd = cmd.ExecuteReader())
+                {
+                    while (rd.Read())
+                    {
+                        cliente.Id = (int)rd["Id"];
+                        cliente.Nome = rd["Nome"].ToString();
+                        cliente.CPF = rd["CPF"].ToString();
+                        cliente.RG = rd["RG"].ToString();
+                        cliente.Email = rd["Email"].ToString();
+                        cliente.Fone = rd["Fone"].ToString();
+
+                    }
+                }
+                return cliente;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("", ex) { Data = { { "Id", 18 } } };
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+        public Cliente BuscarPorCPF(string _CPF)
+        {
+            return null;
+        }
+        public void Alterar(Cliente _cliente)
+        {
+
+        }
+        public void Excluir(int _id)
+        {
+
+        }
+
+    }
+}
